55aa6b395109b26d5a87b4451c9ddf1bd45f6cac
gayathri <gayathriparavii999@gmail.com>
2025-04-18 01:40:57 +0530

    Day12✏️

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,20 @@ import Header from './components/header.jsx'
 import ProductCard from './components/product-card.jsx'
 import AdminPage from './pages/adminPage.jsx';
 import LoginPage from './pages/loginPage.jsx'
+import Testing from './pages/testing.jsx';
+import { Toaster } from 'react-hot-toast';
 
 function App() {
   
 
   return (
     <BrowserRouter>
+    <Toaster position='top-right'/>
        <Routes path="/*">
        <Route path="/admin/*" element={<AdminPage />}/>
        <Route path="/login" element={<LoginPage />}/>
+       <Route path="/testing" element={<Testing />}></Route>
+
        <Route path="/" element={<h1>Home</h1> }/>
        <Route path="/*" element={<h1>404 not found</h1>} />
 
--- src/pages/adminPage.jsx
+++ src/pages/adminPage.jsx
@@ -4,6 +4,7 @@ import { MdWarehouse } from "react-icons/md";
 import { FaFileInvoice } from "react-icons/fa6";
 
 export default function AdminPage() {
+    //
     return (
         <div className="w-full h-screen bg-gray-200 flex p-2">
             <div className="h-full w-[300px] ">
